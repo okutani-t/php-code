1fb8518c479561b849ffebb88b874aba1969a33f
okutani <okutani_taisuke@mut.co.jp>
2016-02-29 11:58:03 +0900

    add masonry

--- image-uploader/index.php
+++ image-uploader/index.php
@@ -59,6 +59,10 @@ $images = $uploader->getImages();
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
     <link rel="stylesheet" href="style.css">
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+    <![endif]-->
 </head>
 <body>
     <!-- header -->
@@ -81,7 +85,7 @@ $images = $uploader->getImages();
 
         <div class="row">
             <!-- upload button -->
-            <div class="col l2 s4">
+            <div class="col m3 s12 upload-box">
                 <form action="" method="post" enctype="multipart/form-data" id="my_form">
                     <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo h(MAX_FILE_SIZE); ?>">
                     <input type="hidden" name="delPath" value="">
@@ -97,14 +101,14 @@ $images = $uploader->getImages();
 
             <!-- msg -->
             <?php if (isset($success)) : ?>
-                <div class="col offset-l1 l8 s8 msg success card-panel teal lighten-2">
+                <div class="col offset-m1 m7 s12 msg success card-panel teal lighten-2">
                     <h4 class="white-text center-align">
                         <?php echo h($success); ?>
                     </h4>
                 </div>
             <?php endif; ?>
             <?php if (isset($error)) : ?>
-                <div class="col offset-l1 l8 s8 msg error card-panel red lighten-1">
+                <div class="col offset-m1 m7 s12 msg error card-panel red lighten-1">
                     <h4 class="white-text center-align">
                         <?php echo h($error); ?>
                     </h4>
@@ -113,13 +117,13 @@ $images = $uploader->getImages();
         </div><!-- /row -->
 
         <!-- img list -->
-        <div class="row">
+        <div class="row grid">
             <?php foreach ($images as $image) : ?>
                 <!-- 画像の表示 -->
-                <div class="card-box col l4 m6 s12">
+                <div class="card-box grid-item col l4 m6 s12">
                     <div class="card hoverable">
                         <div class="card-image">
-                            <img class="materialboxed" src="<?php echo h($image); ?>" width="400"alt="Title">
+                            <img class="materialboxed" src="<?php echo h($image); ?>" width="400" alt="Title">
                             <span class="card-title">Title</span>
                         </div>
                         <div class="card-content">
@@ -127,28 +131,30 @@ $images = $uploader->getImages();
                                 <!-- Download btn -->
                                 <div class="col s7">
                                     <a href="<?php echo h($image); ?>" class="w100p download-btn valign w100p waves-effect waves-light btn teil lighten-2"
-                                    data-img-name="<?php echo h($image); ?>" download="<?php echo h(basename($image)); ?>">
-                                    Download
-                                </a>
-                            </div>
-                            <!-- Delete btn -->
-                            <div class="col s5">
-                                <div class="w100p delete-btn waves-effect waves-light btn pink lighten-1"
-                                data-img-name="<?php echo h($image); ?>">
-                                Delete
+                                        data-img-name="<?php echo h($image); ?>" download="<?php echo h(basename($image)); ?>">
+                                        Download
+                                    </a>
+                                </div>
+                                <!-- Delete btn -->
+                                <div class="col s5">
+                                    <div class="w100p delete-btn waves-effect waves-light btn pink lighten-1"
+                                    data-img-name="<?php echo h($image); ?>">
+                                    Delete
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    <?php endforeach ?>
-</div><!-- /img list row -->
+        <?php endforeach ?>
+    </div><!-- /img list row -->
 
 </div><!-- /container -->
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+<script src="https://npmcdn.com/masonry-layout@4.0/dist/masonry.pkgd.min.js"></script>
+<script src="https://npmcdn.com/imagesloaded@4.1/imagesloaded.pkgd.min.js"></script>
 <script>
 $(function(){
     // メッセージのフェードアウト
@@ -161,6 +167,15 @@ $(function(){
     $(".delete-btn").click(function() {
         $("[name=delPath]").val($(this).data("img-name"));
         $("#my_form").submit();
+    });
+    // masonry
+    var $grid = $('.grid');
+    $grid.imagesLoaded(function(){
+        $grid.masonry({
+            itemSelector: '.grid-item',
+            // isFitWidth: true,
+            isAnimated: true
+        });
     });
 });
 
