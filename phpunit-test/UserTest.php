8751c0fd70e54551e5874bf76ef6a600bc34dea7
okutani <MYACCOUNT@DOMAIN>
2016-04-11 18:16:15 +0900

    fix unit

--- phpunit-test/UserTest.php
+++ phpunit-test/UserTest.php
@@ -1,6 +1,6 @@
 <?php
-require_once "vendor/autoload.php";
-require_once "User.php";
+require_once __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/User.php";
 
 class UserTest extends PHPUnit_Framework_TestCase
 {
